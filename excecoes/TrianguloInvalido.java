34dbda9cdcb3b9a89e8a80b9b1bc9573b2ae7e36
Eduardo Amorim <atenas503@gmail.com>
2019-10-01 23:24:35 -0300

    Create TrianguloInvalido.java

---
+++ excecoes/TrianguloInvalido.java
@@ -0,0 +1,44 @@
+package excecoes;
+
+import java.awt.Color;
+
+/**
+ * Essa classe extende Exception e é uma exceção criada para ser lançada ao tentar por um número negativo na base ou na altura
+ * @author Eduardo Amorim dos Santos Araujo
+ * @author Guilherme Trindade Souza
+ */
+public class TrianguloInvalido extends Exception {
+
+	/**
+	 * valtura receberá o valor da altura do triângulo
+	 * vbase receberá o valor da base do triângulo
+	 */
+	String valtura,vbase;
+	
+	/**
+	 * Construtor padrão para classes que extendem Exception e além disso atribui altura à valtura e base à vbase
+	 * @param altura recebe a altura do triângulo
+	 * @param base recebe a base do triângulo
+	 */
+	public TrianguloInvalido(String altura, String base) {
+		super();
+		valtura=altura;
+		vbase=base;
+	}
+	
+	/**
+	 * Forma a mensagem à ser mandada pelo erro dependendo dos valores que estão dando erro
+	 * @return Mensagem à ser mostrada para o usuário em caso de erro
+	 */
+	public String getMessage() {
+		StringBuffer erro = new StringBuffer();
+		erro.append("Deixe o valor das seguintes coisas maior que 0 para continuar: ");
+		if(Integer.parseInt(vbase)<=0) {
+			erro.append(" |Base| ");
+		}if(Integer.parseInt(valtura)<=0) {
+			erro.append(" |Altura| ");
+		}
+		return erro.toString();
+		
+	}
+}
